6d2bebf1584547fb1881a6975f5703a2efc636d0
Konrad <konrad_here@hotmail.com>
2018-01-15 00:49:54 -0800

    get all semester specific urls

--- scripts/db:populate/index.js
+++ scripts/db:populate/index.js
@@ -1,17 +1,76 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
 const db = require('../../db');
 
+const getHrefs = async (url) => {
+  const response = await axios.get(url);
+  const $ = cheerio.load(response.data);
+
+  const links = $('a');
+
+  const getHref = function() {
+    return $(this).attr('href');
+  };
+
+  return links.map(getHref).get();
+}
+
+const getSpecificCourseUrl = (subject, courseNumber, termStart) =>
+  `https://www.uvic.ca/BAN1P/bwckctlg.p_disp_listcrse?term_in=${termStart}&subj_in=${subject}&crse_in=${courseNumber}&schd_in=`
+
+const getAllUrls = async () => {
+  const baseUrl = 'https://web.uvic.ca/calendar2018-01/'
+  const initialUrl = `${baseUrl}courses/`;
+
+  const initialHrefs = await getHrefs(initialUrl);
+
+  const startString = '../CDs/';
+  const endString = '/CTs.html';
+  const subjects = initialHrefs
+    .filter((href) => href.includes(startString) && href.includes(endString))
+    .filter((url, i, arr) => i === arr.indexOf(url))
+    .map((href) => href.slice(startString.length, -endString.length));
+
+  // ex href: '../CDs/ECON/CTs.html'
+  const subjectUrls = subjects.map((subject) => `${baseUrl}CDs/${subject}${endString}`);
+
+  const coursePromises = subjectUrls.map(getHrefs)
+  const coursesHrefs = await Promise.all(coursePromises);
+
+  const semesterUrls = [];
+  for (let i = 0; i < subjects.length; i++) {
+    for (let j = 0; j < coursesHrefs[i].length; j++) {
+      const found = coursesHrefs[i][j].match(/(\d+)\.html/);
+
+      // get the three semester urls
+      if (found) {
+        const courseNumber = found[1];
+
+        semesterUrls.push(getSpecificCourseUrl(subjects[i], courseNumber, '201709'))
+        semesterUrls.push(getSpecificCourseUrl(subjects[i], courseNumber, '201801'))
+        semesterUrls.push(getSpecificCourseUrl(subjects[i], courseNumber, '201805'))
+      }
+    }
+  }
+
+  return semesterUrls;
+}
+
 const populate = async () => {
+  const urls = await getAllUrls();
+  console.log(urls);
+
   /*
    * TODO:
-   *   - Create the user to insert with ("Uvic")
-   *   - Go through every faculty
-   *   - Go through every class
    *   - Get "Bookings" in memory
+   *   - Create the user to insert with ("Uvic") // use cheerio
    *   - Insert building -> room -> booking
    */
 
-  const res = await db.query('SELECT $1::text as message', ['Hello world!']);
-  console.log(res.rows[0].message);
+  // example db usage
+  // const res = await db.query('SELECT $1::text as message', ['Hello world!']);
+  // console.log(res.rows[0].message);
 };
 
 populate();
